11e91d800384daf32e3195086b118dbdc1164636
陳信屹 <ossug.hychen@gmail.com>
2014-06-18 22:01:45 +0800

    add js dependes.

--- cookbooks/grano/recipes/apilib.rb
+++ cookbooks/grano/recipes/apilib.rb
@@ -16,6 +16,16 @@ execute "install bower" do
   not_if "test -e /usr/bin/bower"
 end
 
+execute "install less" do
+  command "npm i -g less@1.7.1"
+  not_if "test -e /usr/bin/less"
+end
+
+execute "install uglify-js@2.4.14" do
+  command "npm i -g less@1.2.6"
+  not_if "test -e /usr/bin/less"
+end
+
 git "/opt/grano/grano" do
   repository "git://github.com/granoproject/grano.git"
   enable_submodules true
